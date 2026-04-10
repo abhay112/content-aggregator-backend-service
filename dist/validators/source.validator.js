df272e09014c0d6460483d9c2bb1c49c98c9f5ae
abhay112 <sattibhai501@gmail.com>
2026-04-10 09:51:38 +0530

    removed dist

--- dist/validators/source.validator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createSourceSchema = void 0;
-const zod_1 = require("zod");
-exports.createSourceSchema = zod_1.z.object({
-    name: zod_1.z.string().min(1, 'Source name is required'),
-    url: zod_1.z.string().url('Invalid URL format')
-});
